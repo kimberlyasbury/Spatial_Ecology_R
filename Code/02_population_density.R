a8b141af60679efd4b28a18a8cd9724e35292c02
kimberlyasbury <kimberly.c.asbury@gmail.com>
2025-10-16 08:23:15 -0700

    Update 02_population_density.R

--- Code/02_population_density.R
+++ Code/02_population_density.R
@@ -9,7 +9,7 @@ bei
 
 plot(bei)
 
-plot(bei, pch=15, cex-5)
+plot(bei, pch=15, cex.5)
 
 bei.extra
 
@@ -32,3 +32,78 @@ beidens <- density(bei)
 plot(beidens)
 points(bei)
 
+# Plotting the maps one beside the other
+par(mfrow=c(1,2))
+plot(elevation2)
+plot(densitymap)
+
+# Exercise: make a multiframe with maps one on top of the other
+par(mfrow=c(2,1))
+plot(elevation2)
+plot(densitymap)
+
+# one frined to clear graphs
+dev.off()
+plot(elevation2)
+
+# Changin colors to maps
+cl <- colorRampPalette(c("red", "orange", "yellow"))(3)
+plot(densitymap, col=cl)
+
+cl <- colorRampPalette(c("red", "orange", "yellow"))(10)
+plot(densitymap, col=cl)
+
+cl <- colorRampPalette(c("red", "orange", "yellow"))(100)
+plot(densitymap, col=cl)
+
+# search your browser for "colors in R" 
+# http://www.stat.columbia.edu/~tzheng/files/Rcolor.pdf
+
+# Exercise: change the color ramp palette using different colors
+
+cln <- colorRampPalette(c("purple1", "orchid2", "palegreen3", "paleturquoise"))(100)
+plot(densitymap, col=cln)
+
+# Exrcise: build a multiframe and plot the densitymap with two different color ramp palettes one beside the other
+
+par(mfrow=c(1,2))
+
+cln <- colorRampPalette(c("purple1", "orchid2", "palegreen3", "paleturquoise"))(100)
+plot(densitymap, col=cln)
+
+clg <- colorRampPalette(c("green4", "green3", "green2", "green1", "green"))(100)
+plot(densitymap, col=clg)
+
+dev.off()
+
+par(mfrow=c(1,2))
+plot(elevation2)
+plot(densitymap)
+ 
+par(mfrow=c(2,1))
+plot(elevation2)
+plot(densitymap)
+ 
+dev.off()
+
+plot(elevation2)
+cl <- colorRampPalette(c("red", "orange", "yellow"))(3)
+plot(densitymap, col=cl)
+cl <- colorRampPalette(c("red", "orange", "yellow"))(10)
+plot(densitymap, col=cl)
+cl <- colorRampPalette(c("red", "orange", "yellow"))(100)
+plot(densitymap, col=cl)
+cl <- colorRampPalette(c("darkolivegreen", "darkseagreen4", "aquamarine"))(100)
+plot(densitymap, col=cl)
+
+
+cl <- colorRampPalette(c("darkolivegreen", "darkseagreen4", "aquamarine"))(100)
+plot(densitymap, col=cl)
+cln <- colorRampPalette(c("purple1", "orchid2", "palegreen3", "paleturquoise"))(100)
+plot(densitymap, col=cln)
+
+par(mfrow=c(2,2))
+plot(bei)
+plot(el)
+plot(densitymap, col=cl)
+plot(densitymap, col=cln)
